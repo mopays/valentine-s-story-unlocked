9c260fe4092fd0323ec4d35d2c2902a51a3109ea
folk <folkza1755@gmail.com>
2026-02-04 14:56:24 +0700

    edit config vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/valentine-s-story-unlocked/",
+
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    hmr: { overlay: false },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
